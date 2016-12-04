3db15f58bb2b66128578093011666e3c69690c1b
Bobby Powers <bobbypowers@gmail.com>
2016-12-04 18:33:53 -0500

    x86: convert instruction list to bytes

--- X86.ml
+++ X86.ml
@@ -62,10 +62,34 @@ type instr =
   | Jmp of rm         (* excludes relative jumps *)
 [@@deriving show]
 
+let w2w (w: int): char =
+  Char.chr (w land 0xff)
+
+let ximm (w: int): char list =
+  [w2w w; w2w (w lsr 8); w2w (w lsr 16); w2w (w lsr 24)]
+
+let instr_to_string (i: instr) : char list =
+  match i with
+  | Binop (Mov, R_RM (EAX, Reg EDI)) -> ['\x8B'; '\x07']
+  | Binop (Add, RM_I (Reg EDI, imm)) -> if imm > 255;
+                                        then failwith "ADD RM_I immediate too big"
+                                        else ['\x83'; '\xC7'; w2w imm]
+  | Binop (Cmp, R_RM (EAX, Reg EDI)) -> ['\x3B'; '\x07']
+  | Binop (Sub, R_RM (EAX, Reg EDI)) -> ['\x2B'; '\x07']
+  | Xchg (Reg EDI, EAX)              -> ['\x87'; '\x07']
+  | Jcc (ALWAYS, imm)                -> ['\xe9'] @ (ximm imm)
+  | Jcc (E, imm)                     -> ['\x0F'; '\x84'] @ (ximm imm)
+  | Jcc (B, imm)                     -> ['\x0F'; '\x82'] @ (ximm imm)
+  | Jmp (Reg EDX)                    -> ['\xFF'; '\xE2']
+  | _ -> failwith (Printf.sprintf "don't know how to encode %s" (show_instr i))
+
+let rec to_string (instrs: instr list): char list =
+  List.map instr_to_string instrs |> List.flatten
+
 let rec encode (a: int) (cs: prog): instr list =
   (* FIXME: needs to be length of bytes, not # of x86 instructions *)
   let rec xenc_length c =
-    List.length (xenc (fun x -> 0) c)
+    List.length (to_string (xenc (fun x -> 0) c))
   and xenc (t: int -> int) (c: Syntax.instr): instr list =
     match c with
     | Pop    -> [Binop (Mov, R_RM (EAX, Reg EDI));
@@ -75,11 +99,11 @@ let rec encode (a: int) (cs: prog): instr list =
     | Push i -> [Binop (Sub, RM_I (Reg EDI, 4));
 		 Binop (Mov, RM_R (Reg EDI, EAX));
 		 Binop (Mov, RM_I (Reg EAX, i))]
-    | Jump i -> [Jcc (ALWAYS, (t i))]
+    | Jump i -> [Jcc (ALWAYS, t i)]
     | Jeq i  -> [Binop (Cmp, R_RM (EAX, Reg EDI));
-		 Jcc (E, (t i))]
+		 Jcc (E, t i)]
     | Jlt i  -> [Binop (Cmp, R_RM (EAX, Reg EDI));
-		 Jcc (B, (t i))]
+		 Jcc (B, t i)]
     | Stop   -> [Jmp (Reg EDX)]
   in
   let rec addr cs a p: int =
@@ -88,4 +112,4 @@ let rec encode (a: int) (cs: prog): instr list =
     | [], a, _      -> a
     | c :: cs, a, p -> addr cs (a + xenc_length c) (p - 1)
   in
-  List.flatten (List.map (fun c -> xenc (addr cs a) c) cs)
+  List.map (fun c -> xenc (addr cs a) c) cs |> List.flatten
