2470b7f1eb5daf4d0e3cd60f1365b6515981ef63
Bobby Powers <bobbypowers@gmail.com>
2016-12-18 18:40:00 -0500

    verify: stub out some more

--- Verify.ml
+++ Verify.ml
@@ -1,4 +1,7 @@
 open Core.Std
+open Smtlib
+
+let z3_path = "./z3"
 
 (* Similar to how exec/next in Semantics represent the PC as an
    int, we represent paths as sequences of offsets into instrs *)
@@ -31,21 +34,29 @@ let rec all_paths (instrs : X86.instr list) (pc: int) (curr : int list): int lis
   | Jmp (reg)         -> [pc :: curr] (* from Stop instruction *)
   | _                 ->  pc :: curr |> all_paths instrs (pc + 1)
 
-let verify_path (instrs: X86.instr list)
+let rec strongest_postcondition () =
+  ()
+
+let verify_path (solver : Smtlib.solver)
+                (instrs: X86.instr list)
                 (xs : int list)
                 (max_stack_depth : int)
                 (pcs : int list): (unit, Problem.t) Result.t =
   Printf.printf "path: [";
   List.iter ~f:(fun (i : int) -> Printf.printf "%d; " i) pcs;
   Printf.printf "]\n";
+  (* setup precondition (relate xs + max_stack_depth to value in edi *)
+  (* setup postcondition pc = edx, edi not overflowed/underflowed *)
+  (* pass both to recursive verify *)
   Error (Problem.VerificationFailed "not implemented")
 
 let stack_maintenance (instrs : X86.instr list)
                       (xs : int list)
                       (max_stack_depth : int): (unit, Problem.t) Result.t =
+  let solver = Smtlib.make_solver z3_path in
   all_paths instrs 0 []
   |> List.map ~f:List.rev
-  |> List.map ~f:(verify_path instrs xs max_stack_depth)
+  |> List.map ~f:(verify_path solver instrs xs max_stack_depth)
   |> List.fold_left ~init:(Ok ()) ~f:(fun r ok -> match r with
                                                   | Error err -> Error err
                                                   | _ -> ok)
